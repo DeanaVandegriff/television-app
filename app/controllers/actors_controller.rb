1e985efa0fa2585991420a9526525a447b6487bd
deana <dvandegriff1@gmail.com>
2022-04-15 10:01:13 -0500

    adds RESTful actions and tests through requests.http

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,7 +8,7 @@ class ActorsController < ApplicationController
     actor = Actor.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
-      known_for: params[:known_for],
+      known_for: params[:known_for]
     )
     actor.save
     render json: actor.as_json
@@ -21,8 +21,8 @@ class ActorsController < ApplicationController
 
   def update
     actor = Actor.find_by(id: params[:id])
-    actor.first_name = params[:first_name] || actor.first_name,
-    actor.last_name = params[:last_name] || actor.last_name,
+    actor.first_name = params[:first_name] || actor.first_name
+    actor.last_name = params[:last_name] || actor.last_name
     actor.known_for = params[:known_for] || actor.known_for
     actor.save
     render json: actor.as_json
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,37 @@
 class ShowsController < ApplicationController
+  def index
+    shows = Show.all
+    render json: shows.as_json
+  end
+
+  def create
+    show = Show.new(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+    )
+    show.save
+    render json: show.as_json
+  end
+
+  def show
+    show = Show.find_by(id: params[:id])
+    render json: show.as_json
+  end
+
+  def update
+    show = Show.find_by(id: params[:id])
+    show.title = params[:title] || show.title
+    show.year = params[:year] || show.year
+    show.plot = params[:plot] || show.plot
+    show.save
+    render json: show.as_json
+  end
+
+  def destroy
+    show = Show.find_by(id: params[:id])
+    show.destroy
+    render json: {message: "show removed"}
+  end
+
 end
